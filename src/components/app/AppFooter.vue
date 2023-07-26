e05a912f2bcf06ab024273a640d41bf33cc0d320
sfxcode <tom@sfxcode.com>
2023-07-26 16:45:17 +0200

    chore(dependencies): PrimeVue 3.31

--- src/components/app/AppFooter.vue
+++ src/components/app/AppFooter.vue
@@ -4,7 +4,7 @@ const version = ref(import.meta.env.VITE_APP_VERSION)
 
 <template>
   <div class="mt-4 layout-footer">
-    <span class="font-medium ml-2">Formkit (0.17) - PrimeVue (3.29) - Version {{ version }}</span>
+    <span class="font-medium ml-2">Formkit (0.17) - PrimeVue (3.31) - Version {{ version }}</span>
   </div>
 </template>
 
