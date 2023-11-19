85113dcd699811ea556741c04342c2c2e9c48474
sfxcode <tom@sfxcode.com>
2023-11-19 09:06:56 +0100

    docs(versions): Update formkit and primevue versions in footer

--- src/components/app/AppFooter.vue
+++ src/components/app/AppFooter.vue
@@ -4,7 +4,7 @@ const version = ref(import.meta.env.VITE_APP_VERSION)
 
 <template>
   <div class="mt-4 layout-footer">
-    <span class="font-medium ml-2">Formkit (1.2.2) - PrimeVue (3.36) - Version {{ version }}</span>
+    <span class="font-medium ml-2">Formkit (1.3) - PrimeVue (3.40) - Version {{ version }}</span>
   </div>
 </template>
 
