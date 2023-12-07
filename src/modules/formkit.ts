959e8a74c1898f3a058b2aa32bd81f2bbe6e7bbf
sfxcode <tom@sfxcode.com>
2023-12-07 17:51:41 +0100

    chore(feature): add autoAnimate plugin

--- src/modules/formkit.ts
+++ src/modules/formkit.ts
@@ -1,16 +1,34 @@
 import { defaultConfig, plugin } from '@formkit/vue'
+import { createAutoAnimatePlugin } from '@formkit/addons'
 
 import { de, en } from '@formkit/i18n'
 import type { UserModule } from '@/types'
 import { primeInputs } from '@/formkit'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const install: UserModule = ({ app, router, isClient }) => {
+export const install: UserModule = ({ app }) => {
   app.use(plugin, defaultConfig({
     locales: { de, en },
     // Define the active locale
     locale: 'en',
     inputs: primeInputs,
+    plugins: [
+      createAutoAnimatePlugin(
+        {
+          /* optional AutoAnimate config */
+          // default:
+          duration: 250,
+          easing: 'ease-in-out',
+          delay: 0,
+        },
+        {
+          /* optional animation targets object */
+          // default:
+          global: ['outer', 'inner'],
+          form: ['form'],
+          repeater: ['items'],
+        },
+      ),
+    ],
   }))
 }
 
