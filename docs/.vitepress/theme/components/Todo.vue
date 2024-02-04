fdb639908adc7c7a2d2b88628fceb10ad9fa616e
sfxcode <tom@sfxcode.com>
2024-02-04 12:46:14 +0100

    docs(icons): Use prime-icons

--- docs/.vitepress/theme/components/Todo.vue
+++ docs/.vitepress/theme/components/Todo.vue
@@ -4,8 +4,10 @@
 <template>
   <div>
     <div class="mt-8 flex gap-2">
-      <div class="i-mdi-information text-2xl mb-2 text-[color:var(--vp-c-brand)]"/>
-      <div class="text-2xl">TODO</div>
+      <div class="pi-info-circle text-2xl mb-2 text-[color:var(--vp-c-brand)]" />
+      <div class="text-2xl">
+        TODO
+      </div>
     </div>
     <div class="">
       <span>More Info to come ...</span>
