24f6df31040ae50ca24a196116da3ddcd13bd240
sfxcode <tom@sfxcode.com>
2024-07-30 07:13:55 +0200

    refactor: use only p-formkit-data-debug for styling

--- src/components/FormKitDebug.vue
+++ src/components/FormKitDebug.vue
@@ -15,8 +15,8 @@ defineProps({
   <div class="p-formkit-data-debug">
     <h3>{{ header }}</h3>
     <slot />
-    <pre v-if="data" class="text-xs">{{ data }}</pre>
-    <span v-else class="text-xs">No Data available</span>
+    <pre v-if="data">{{ data }}</pre>
+    <span v-else>No Data available</span>
   </div>
 </template>
 
