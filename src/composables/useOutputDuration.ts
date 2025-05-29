a9c1252b48bf6aa7837748d1449d65b4bea9fbec
sfxcode <tom@sfxcode.com>
2025-05-29 18:44:53 +0200

    refactor: make h and m case insensitive

--- src/composables/useOutputDuration.ts
+++ src/composables/useOutputDuration.ts
@@ -1,43 +1,40 @@
 export function useOutputDuration() {
   function durationToMinutes(duration: string): number {
-    let hours = 0
-    let minutes = 0
-    if (duration.includes(':')) {
-      hours = +duration.substring(0, duration.indexOf(':')).trim()
-      minutes = +duration.substring(duration.indexOf(':') + 1).trim()
+    let hours = 0; let minutes = 0
+    const lowerDuration = duration.toLowerCase()
+
+    if (lowerDuration.includes(':')) {
+      [hours, minutes] = lowerDuration.split(':').map(part => +part.trim())
     }
     else {
-      let durationString = duration
-      if (duration.includes('h')) {
-        hours = +duration.substring(0, duration.indexOf('h')).trim()
-        durationString = duration.substring(duration.indexOf('h') + 1)
+      if (lowerDuration.includes('h')) {
+        hours = +lowerDuration.split('h')[0].trim()
+        const remainder = lowerDuration.split('h')[1] || ''
+
+        if (remainder.includes('m'))
+          minutes = +remainder.split('m')[0].trim()
+        else if (/^\d+$/.test(remainder))
+          minutes = +remainder
       }
-      if (durationString.includes('m'))
-        minutes = +durationString.substring(0, durationString.indexOf('m')).trim()
-      if (durationString === durationString.replace(/\D/g, ''))
-        minutes = +durationString
+      else if (lowerDuration.includes('m')) {
+        minutes = +lowerDuration.split('m')[0].trim()
+      }
+      else if (/^\d+$/.test(lowerDuration)) {
+        minutes = +lowerDuration
+      }
     }
+
     return hours * 60 + minutes
   }
 
   function formattedDuration(duration: string): string {
     const minutes = durationToMinutes(duration)
+    const hours = Math.trunc(minutes / 60)
+    const remainingMinutes = minutes % 60
 
-    const stunden = Math.trunc(minutes / 60)
-    const minuten = minutes % 60
-
-    let result = ''
-
-    if (stunden > 0)
-      result = `${result} ${stunden}h`
-    if (minuten > 0)
-      result = `${result} ${minuten}m`
     if (minutes === 0)
-      result = '0'
-
-    if (result.length === 0)
-      result = duration
-    return result.trim()
+      return '0'
+    return `${hours > 0 ? `${hours}h` : ''}${hours > 0 && remainingMinutes > 0 ? ' ' : ''}${remainingMinutes > 0 ? `${remainingMinutes}m` : ''}`
   }
 
   return { durationToMinutes, formattedDuration }
