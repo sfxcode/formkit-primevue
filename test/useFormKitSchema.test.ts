fa70814e4ba7fed877c2bdadb4517aeb861e2552
sfxcode <tom@sfxcode.com>
2025-05-29 20:28:07 +0200

    style(linting): Fix error

--- test/useFormKitSchema.test.ts
+++ test/useFormKitSchema.test.ts
@@ -5,24 +5,24 @@ it('add list group', () => {
   const { addListGroup } = useFormKitSchema()
 
   const listGroup = addListGroup()
-  expect(listGroup.$formkit).toBe('group')
-  expect(listGroup.if).toBe('true')
-  expect(listGroup.for).toEqual(['item', 'index', '$items'])
-  expect(listGroup.key).toBe('$item')
-  expect(listGroup.index).toBe('$index')
+  expect(listGroup?.$formkit).toBe('group')
+  expect(listGroup?.if).toBe('true')
+  expect(listGroup?.for).toEqual(['item', 'index', '$items'])
+  expect(listGroup?.key).toBe('$item')
+  expect(listGroup?.index).toBe('$index')
 })
 
 it('add element with non-boolean render value', () => {
   const { addElement } = useFormKitSchema()
   const element = addElement('div', [], {}, '$context.visible')
-  expect(element.if).toBe('$context.visible')
+  expect(element?.if).toBe('$context.visible')
 })
 
 it('add component with props', () => {
   const { addComponent } = useFormKitSchema()
   const props = { label: 'Save', severity: 'primary' }
   const component = addComponent('Button', props)
-  expect(component.props).toEqual(props)
+  expect(component?.props).toEqual(props)
 })
 
 describe('addElementsInOuterDiv', () => {
@@ -30,30 +30,30 @@ describe('addElementsInOuterDiv', () => {
     const { addElementsInOuterDiv } = useFormKitSchema()
     const outerDiv = addElementsInOuterDiv()
 
-    expect(outerDiv.$el).toBe('div')
-    expect(outerDiv.attrs.class).toBe('formkit-outer ')
+    expect(outerDiv?.$el).toBe('div')
+    expect(outerDiv?.attrs?.class).toBe('formkit-outer ')
 
-    const wrapperDiv = outerDiv.children[0]
-    expect(wrapperDiv.$el).toBe('div')
-    expect(wrapperDiv.attrs.class).toBe('formkit-wrapper')
+    const wrapperDiv = outerDiv?.children?.[0]
+    expect(wrapperDiv?.$el).toBe('div')
+    expect(wrapperDiv?.attrs?.class).toBe('formkit-wrapper')
 
-    const labelDiv = wrapperDiv.children[0]
-    expect(labelDiv.$el).toBe('label')
-    expect(labelDiv.children).toEqual([''])
+    const labelDiv = wrapperDiv?.children?.[0]
+    expect(labelDiv?.$el).toBe('label')
+    expect(labelDiv?.children).toEqual([''])
 
-    const innerDiv = wrapperDiv.children[1]
-    expect(innerDiv.$el).toBe('div')
-    expect(innerDiv.attrs.class).toBe('formkit-inner ')
+    const innerDiv = wrapperDiv?.children?.[1]
+    expect(innerDiv?.$el).toBe('div')
+    expect(innerDiv?.attrs?.class).toBe('formkit-inner ')
   })
 
   it('applies custom classes', () => {
     const { addElementsInOuterDiv } = useFormKitSchema()
     const outerDiv = addElementsInOuterDiv([], 'custom-inner', 'custom-outer')
 
-    expect(outerDiv.attrs.class).toBe('formkit-outer custom-outer')
+    expect(outerDiv?.attrs?.class).toBe('formkit-outer custom-outer')
 
-    const innerDiv = outerDiv.children[0].children[1]
-    expect(innerDiv.attrs.class).toBe('formkit-inner custom-inner')
+    const innerDiv = outerDiv?.children?.[0]?.children?.[1]
+    expect(innerDiv?.attrs?.class).toBe('formkit-inner custom-inner')
   })
 })
 
@@ -62,9 +62,9 @@ it('combines formKitAttrs with element properties', () => {
   const formKitAttrs = { validation: 'required', validationVisibility: 'dirty' }
   const element = addElement('div', [], {}, true, formKitAttrs)
 
-  expect(element.$el).toBe('div')
-  expect(element.validation).toBe('required')
-  expect(element.validationVisibility).toBe('dirty')
+  expect(element?.$el).toBe('div')
+  expect(element?.validation).toBe('required')
+  expect(element?.validationVisibility).toBe('dirty')
 })
 
 it('combines formKitAttrs with component properties', () => {
@@ -72,7 +72,7 @@ it('combines formKitAttrs with component properties', () => {
   const formKitAttrs = { validation: 'required', validationLabel: 'Button' }
   const component = addComponent('Button', {}, true, formKitAttrs)
 
-  expect(component.$cmp).toBe('Button')
-  expect(component.validation).toBe('required')
-  expect(component.validationLabel).toBe('Button')
+  expect(component?.$cmp).toBe('Button')
+  expect(component?.validation).toBe('required')
+  expect(component?.validationLabel).toBe('Button')
 })
