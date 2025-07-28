a44d2a2c8fe705986f858d037879d7f4a9da4033
sfxcode <tom@sfxcode.com>
2025-07-28 23:08:18 +0200

    feat: add demo label plugin for FormKit nodes with help text support by title and hide help section

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -24,3 +24,39 @@ export function addPrimeAsteriskPlugin(node: FormKitNode): void {
     }
   })
 }
+
+export function addLabelPlugin(node: FormKitNode): void {
+  if (!node.props.type.startsWith('prime'))
+    return
+
+  node.on('created', () => {
+    if (node.props.definition?.schema) {
+      const schemaFn = node.props.definition?.schema as FormKitExtendableSchemaRoot
+      node.props.definition!.schema = (sectionsSchema = {}) => {
+        sectionsSchema.label = {
+          children: [{
+            $el: 'span',
+            attrs: {
+              title: '$help',
+              class: 'p-formkit-label',
+            },
+            children: ['$label'],
+          }, {
+            $el: 'span',
+            if: '$state.required',
+            attrs: {
+              class: 'p-formkit-required',
+              title: '$help',
+            },
+            children: ['*'],
+          }],
+        }
+        sectionsSchema.help = {
+          children: [],
+        }
+
+        return schemaFn(sectionsSchema)
+      }
+    }
+  })
+}
