bc418620304501869fca2a38f7fb9b03c5ce11f1
sfxcode <tom@sfxcode.com>
2025-09-02 20:46:50 +0200

    chore: use linting

--- dev/pages/inputs/DatePicker.vue
+++ dev/pages/inputs/DatePicker.vue
@@ -2,47 +2,47 @@
 const primeAttributes = 'dateFormat, showIcon, icon, .... (see documentation)'
 
 const schema
-    = [
-      {
-        $formkit: 'primeDatePicker',
-        id: 'basic',
-        name: 'basic',
-        label: 'Basic',
-        placeholder: 'MM/DD/YYYY',
-        validation: 'required',
-      },
-      {
-        $formkit: 'primeDatePicker',
-        name: 'styled',
-        label: 'Styled',
-        style: { background: 'gray' },
-        class: 'customClass',
-        showIcon: true,
-      },
-      {
-        $formkit: 'primeDatePicker',
-        name: 'icon',
-        label: 'Custom Icon',
-        dateFormat: 'yy-mm',
-        showIcon: true,
-        icon: 'pi pi-question',
-      },
-      {
-        $formkit: 'primeDatePicker',
-        name: 'range',
-        label: 'Date Range',
-        selectionMode: 'range',
-        manualInput: false,
-      },
-      {
-        $formkit: 'primeDatePicker',
-        name: 'multiple',
-        label: 'Multiple Dates',
-        selectionMode: 'multiple',
-        manualInput: false,
-      },
+  = [
+    {
+      $formkit: 'primeDatePicker',
+      id: 'basic',
+      name: 'basic',
+      label: 'Basic',
+      placeholder: 'MM/DD/YYYY',
+      validation: 'required',
+    },
+    {
+      $formkit: 'primeDatePicker',
+      name: 'styled',
+      label: 'Styled',
+      style: { background: 'gray' },
+      class: 'customClass',
+      showIcon: true,
+    },
+    {
+      $formkit: 'primeDatePicker',
+      name: 'icon',
+      label: 'Custom Icon',
+      dateFormat: 'yy-mm',
+      showIcon: true,
+      icon: 'pi pi-question',
+    },
+    {
+      $formkit: 'primeDatePicker',
+      name: 'range',
+      label: 'Date Range',
+      selectionMode: 'range',
+      manualInput: false,
+    },
+    {
+      $formkit: 'primeDatePicker',
+      name: 'multiple',
+      label: 'Multiple Dates',
+      selectionMode: 'multiple',
+      manualInput: false,
+    },
 
-    ]
+  ]
 
 const data = {}
 </script>
--- dev/pages/styling/Grid.vue
+++ dev/pages/styling/Grid.vue
@@ -2,56 +2,56 @@
 import { FormKitDataEdit, FormKitDataView } from 'my-library-components'
 
 const schema
-    = [
-      {
-        $formkit: 'primeInputText',
-        name: 'name',
-        label: 'col-8',
-        help: 'Required.',
-        validation: 'required',
-        outerClass: 'col-8',
-      },
-      {
-        $formkit: 'primeInputText',
-        name: 'name2',
-        label: 'col-4',
-        outerClass: 'col-4',
-      },
-      {
-        $formkit: 'primeInputNumber',
-        name: 'name3',
-        label: 'col-4',
-        outerClass: 'col-4',
+  = [
+    {
+      $formkit: 'primeInputText',
+      name: 'name',
+      label: 'col-8',
+      help: 'Required.',
+      validation: 'required',
+      outerClass: 'col-8',
+    },
+    {
+      $formkit: 'primeInputText',
+      name: 'name2',
+      label: 'col-4',
+      outerClass: 'col-4',
+    },
+    {
+      $formkit: 'primeInputNumber',
+      name: 'name3',
+      label: 'col-4',
+      outerClass: 'col-4',
 
-      },
+    },
 
-    ]
+  ]
 
 const outputSchema
-    = [
-      {
-        $formkit: 'primeOutputText',
-        name: 'name',
-        label: 'col-9',
-        help: 'Required.',
-        validation: 'required',
-        outerClass: 'col-9',
-      },
-      {
-        $formkit: 'primeOutputText',
-        name: 'name2',
-        label: 'col-4',
-        outerClass: 'col-4',
-      },
-      {
-        $formkit: 'primeOutputNumber',
-        name: 'name3',
-        label: 'col-4',
-        outerClass: 'col-4',
+  = [
+    {
+      $formkit: 'primeOutputText',
+      name: 'name',
+      label: 'col-9',
+      help: 'Required.',
+      validation: 'required',
+      outerClass: 'col-9',
+    },
+    {
+      $formkit: 'primeOutputText',
+      name: 'name2',
+      label: 'col-4',
+      outerClass: 'col-4',
+    },
+    {
+      $formkit: 'primeOutputNumber',
+      name: 'name3',
+      label: 'col-4',
+      outerClass: 'col-4',
 
-      },
+    },
 
-    ]
+  ]
 
 const data = { name: 'Hey Some Value', name2: 'Another value !', name3: 42 }
 </script>
