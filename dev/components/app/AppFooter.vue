126e9452f2160f51ff922d16ef19b2a02b8c9513
sfxcode <tom@sfxcode.com>
2025-10-14 22:45:16 +0200

    refactor(AppFooter): update PrimeVue version and year in footer

--- dev/components/app/AppFooter.vue
+++ dev/components/app/AppFooter.vue
@@ -4,7 +4,7 @@ const version = ref(import.meta.env.VITE_APP_VERSION)
 
 <template>
   <div class="mt-8 layout-footer text-center text-gray-700">
-    <span class="font-medium ml-2">Formkit (1.6) - PrimeVue (4.2.x) - FormKit-PrimeVue-Version {{ version }} - sfxcode 2024</span>
+    <span class="font-medium ml-2">Formkit (1.6) - PrimeVue (4.4.x) - FormKit-PrimeVue-Version {{ version }} - sfxcode 2025</span>
   </div>
 </template>
 
