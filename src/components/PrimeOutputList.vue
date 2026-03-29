80c62715f1fca2c667a27c6144b21a8264a04319
sfxcode <tom@sfxcode.com>
2026-03-29 13:56:40 +0200

    feat: enhance PrimeOutputList with additional props and improve template structure

--- src/components/PrimeOutputList.vue
+++ src/components/PrimeOutputList.vue
@@ -1,14 +1,18 @@
-<script setup lang='ts'>
+<script setup lang="ts">
 import type { FormKitFrameworkContext } from '@formkit/core'
+import type { SelectProps } from 'primevue/select'
 import type { PropType } from 'vue'
 import type { FormKitIconProps } from './FormKitIcon.vue'
 import { computed } from 'vue'
-import { useFormKitSection } from '../composables'
+import { useFormKitSection } from '../composables/index.ts'
 import FormKitIcon from './FormKitIcon.vue'
 import FormKitPrefix from './FormKitPrefix.vue'
 import FormKitSuffix from './FormKitSuffix.vue'
 
 export interface PrimeOutputListProps {
+  options?: SelectProps['options']
+  optionLabel?: SelectProps['optionLabel']
+  optionValue?: SelectProps['optionValue']
   convertValue?: (array: []) => []
 }
 
@@ -27,7 +31,14 @@ const { hasPrefix, hasPrefixIcon, hasSuffix, hasSuffixIcon } = useFormKitSection
 
 const getListValues = computed(() => {
   const values = Array.isArray(props.context?._value) ? props.context._value : []
-  if (typeof props.context?.convertValue === 'function') {
+  if (props.context?.options && props.context?.optionValue) {
+    const key: string = `${props.context.optionValue}`
+    return values.map((value) => {
+      const foundOption = props.context?.options?.find(option => option[key] === value)
+      return foundOption ? foundOption[props.context?.optionLabel as string] : value
+    })
+  }
+  else if (typeof props.context?.convertValue === 'function') {
     return props.context.convertValue([...values])
   }
   return values
@@ -40,29 +51,60 @@ const listItemsClass = computed(() => props.context?.attrs?.class || '')
 
 <template>
   <div class="p-formkit p-output-list">
-    <FormKitIcon v-if="hasPrefixIcon" :icon-class="context?.iconPrefix as string" :on-click="context?.onIconPrefixClicked as (() => void)" position="prefix" />
+    <FormKitIcon
+      v-if="hasPrefixIcon"
+      :icon-class="context?.iconPrefix as string"
+      :on-click="context?.onIconPrefixClicked as () => void"
+      position="prefix"
+    />
     <FormKitPrefix v-if="hasPrefix && listStyle === 'span'" :prefix="context?.prefix as string" />
-    <span v-if="listStyle === 'span'" :id="context?.id" :style="context?.attrs?.style" class="p-output-list-items" :class="listItemsClass">
+    <span
+      v-if="listStyle === 'span'"
+      :id="context?.id"
+      :style="context?.attrs?.style"
+      class="p-output-list-items"
+      :class="listItemsClass"
+    >
       <template v-for="(value, index) of getListValues" :key="index">
-        <span v-if="index !== 0" class="p-output-list-divider" :class="dividerClass">{{ context?.divider ?? ', ' }}</span>
+        <span v-if="index !== 0" class="p-output-list-divider" :class="dividerClass">{{
+          context?.divider ?? ", "
+        }}</span>
         <span class="p-output-list-item" :class="itemClass">{{ value }}</span>
       </template>
     </span>
-    <div v-if="listStyle === 'div'" :id="context?.id" :style="context?.attrs?.style" class="p-output-list-items" :class="listItemsClass">
+    <div
+      v-if="listStyle === 'div'"
+      :id="context?.id"
+      :style="context?.attrs?.style"
+      class="p-output-list-items"
+      :class="listItemsClass"
+    >
       <template v-for="(value, index) of getListValues" :key="index">
         <div class="p-output-list-item" :class="itemClass">
           {{ value }}
         </div>
       </template>
     </div>
-    <ul v-if="listStyle === 'ul'" :id="context?.id" :style="context?.attrs?.style" class="p-output-list-items" :class="listItemsClass">
+    <ul
+      v-if="listStyle === 'ul'"
+      :id="context?.id"
+      :style="context?.attrs?.style"
+      class="p-output-list-items"
+      :class="listItemsClass"
+    >
       <li v-for="(value, index) of getListValues" :key="index">
         <span class="p-output-list-item" :class="itemClass">
           {{ value }}
         </span>
       </li>
     </ul>
-    <ol v-if="listStyle === 'ol'" :id="context?.id" :style="context?.attrs?.style" class="p-output-list-items" :class="listItemsClass">
+    <ol
+      v-if="listStyle === 'ol'"
+      :id="context?.id"
+      :style="context?.attrs?.style"
+      class="p-output-list-items"
+      :class="listItemsClass"
+    >
       <li v-for="(value, index) of getListValues" :key="index">
         <span class="p-output-list-item" :class="itemClass">
           {{ value }}
@@ -70,6 +112,11 @@ const listItemsClass = computed(() => props.context?.attrs?.class || '')
       </li>
     </ol>
     <FormKitSuffix v-if="hasSuffix && listStyle === 'span'" :suffix="context?.suffix as string" />
-    <FormKitIcon v-if="hasSuffixIcon" :icon-class="context?.iconSuffix as string" :on-click="context?.onIconSuffixClicked as (() => void)" position="suffix" />
+    <FormKitIcon
+      v-if="hasSuffixIcon"
+      :icon-class="context?.iconSuffix as string"
+      :on-click="context?.onIconSuffixClicked as () => void"
+      position="suffix"
+    />
   </div>
 </template>
--- src/definitions/output.ts
+++ src/definitions/output.ts
@@ -10,39 +10,120 @@ import PrimeOutputReference from '../components/PrimeOutputReference.vue'
 import PrimeOutputText from '../components/PrimeOutputText.vue'
 
 export const primeOutputTextDefinition: FormKitTypeDefinition = createInput(PrimeOutputText, {
-  props: ['prefix', 'suffix', 'iconPrefix', 'iconSuffix', 'isTranslationKey', 'html', 'onIconPrefixClicked', 'onIconSuffixClicked', 'convertValue', 'maxLength'],
+  props: [
+    'prefix',
+    'suffix',
+    'iconPrefix',
+    'iconSuffix',
+    'isTranslationKey',
+    'html',
+    'onIconPrefixClicked',
+    'onIconSuffixClicked',
+    'convertValue',
+    'maxLength',
+  ],
 })
 
 export const primeOutputDateDefinition: FormKitTypeDefinition = createInput(PrimeOutputDate, {
-  props: ['prefix', 'suffix', 'iconPrefix', 'iconSuffix', 'onIconPrefixClicked', 'onIconSuffixClicked'],
+  props: [
+    'prefix',
+    'suffix',
+    'iconPrefix',
+    'iconSuffix',
+    'onIconPrefixClicked',
+    'onIconSuffixClicked',
+  ],
 })
 
 export const primeOutputNumberDefinition: FormKitTypeDefinition = createInput(PrimeOutputNumber, {
-  props: ['prefix', 'suffix', 'iconPrefix', 'iconSuffix', 'onIconPrefixClicked', 'onIconSuffixClicked'],
+  props: [
+    'prefix',
+    'suffix',
+    'iconPrefix',
+    'iconSuffix',
+    'onIconPrefixClicked',
+    'onIconSuffixClicked',
+  ],
   family: 'PrimeOutput',
 })
 
 export const primeOutputLinkDefinition: FormKitTypeDefinition = createInput(PrimeOutputLink, {
-  props: ['prefix', 'suffix', 'iconPrefix', 'iconSuffix', 'title', 'onIconPrefixClicked', 'onIconSuffixClicked'],
+  props: [
+    'prefix',
+    'suffix',
+    'iconPrefix',
+    'iconSuffix',
+    'title',
+    'onIconPrefixClicked',
+    'onIconSuffixClicked',
+  ],
   family: 'PrimeOutput',
 })
 
-export const primeOutputReferenceDefinition: FormKitTypeDefinition = createInput(PrimeOutputReference, {
-  props: ['prefix', 'suffix', 'iconPrefix', 'iconSuffix', 'reference', 'internal', 'linkComponentName', 'title', 'onIconPrefixClicked', 'onIconSuffixClicked'],
-  family: 'PrimeOutput',
-})
+export const primeOutputReferenceDefinition: FormKitTypeDefinition = createInput(
+  PrimeOutputReference,
+  {
+    props: [
+      'prefix',
+      'suffix',
+      'iconPrefix',
+      'iconSuffix',
+      'reference',
+      'internal',
+      'linkComponentName',
+      'title',
+      'onIconPrefixClicked',
+      'onIconSuffixClicked',
+    ],
+    family: 'PrimeOutput',
+  },
+)
 
 export const primeOutputBooleanDefinition: FormKitTypeDefinition = createInput(PrimeOutputBoolean, {
-  props: ['prefix', 'suffix', 'iconPrefix', 'iconSuffix', 'trueValue', 'falseValue', 'onIconPrefixClicked', 'onIconSuffixClicked'],
+  props: [
+    'prefix',
+    'suffix',
+    'iconPrefix',
+    'iconSuffix',
+    'trueValue',
+    'falseValue',
+    'onIconPrefixClicked',
+    'onIconSuffixClicked',
+  ],
   family: 'PrimeOutput',
 })
 
-export const primeOutputDurationDefinition: FormKitTypeDefinition = createInput(PrimeOutputDuration, {
-  props: ['prefix', 'suffix', 'iconPrefix', 'iconSuffix', 'onIconPrefixClicked', 'onIconSuffixClicked'],
-  family: 'PrimeOutput',
-})
+export const primeOutputDurationDefinition: FormKitTypeDefinition = createInput(
+  PrimeOutputDuration,
+  {
+    props: [
+      'prefix',
+      'suffix',
+      'iconPrefix',
+      'iconSuffix',
+      'onIconPrefixClicked',
+      'onIconSuffixClicked',
+    ],
+    family: 'PrimeOutput',
+  },
+)
 
 export const primeOutputListDefinition: FormKitTypeDefinition = createInput(PrimeOutputList, {
-  props: ['prefix', 'suffix', 'iconPrefix', 'iconSuffix', 'divider', 'itemClass', 'dividerClass', 'listStyle', 'onIconPrefixClicked', 'onIconSuffixClicked', 'convertValue'],
+  props: [
+    'prefix',
+    'suffix',
+    'iconPrefix',
+    'iconSuffix',
+    'divider',
+    'itemClass',
+    'dividerClass',
+    'listStyle',
+    'onIconPrefixClicked',
+    'onIconSuffixClicked',
+    'optionLabel',
+    'options',
+    'optionValue',
+    'convertValue',
+  ],
   family: 'PrimeOutput',
 })
