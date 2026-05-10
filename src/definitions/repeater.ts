d5435aa607738a6fdba4c2286145b2651cee5d67
sfxcode <tom@sfxcode.com>
2026-05-10 21:45:25 +0200

    chore: refactor button handling in repeater component for improved functionality

--- src/definitions/repeater.ts
+++ src/definitions/repeater.ts
@@ -2,47 +2,52 @@ import type { FormKitNode, FormKitTypeDefinition } from '@formkit/core'
 import { createInput } from '@formkit/vue'
 import { useFormKitSchema } from '../composables/useFormKitSchema'
 
-const { addList, addElement, addListGroup, addComponent, addElementsInOuterDiv } = useFormKitSchema()
+const { addList, addElement, addListGroup, addComponent } = useFormKitSchema()
 
-function addActionButtons(innerClass: string = '', outerClass: string = '', label: string = 'Actions', help: string = '', render: string = 'true') {
-  const addButtonComponent = (onClick: string = '', label: string = '', icon: string = '', severity: string = '', render: string = 'true', disabled: string = 'false', styleClass: string = 'p-button-sm'): object => {
-    return addComponent('Button', { onClick, label, icon, class: styleClass, severity, disabled }, render)
+function addButtonGroup(buttonGroupClass: string = '', buttonGroupItemClass: string = '', buttonSize: string, render: string = 'true') {
+  const addActionButtonComponent = (onClick: string = '', icon: string = '', severity: string = '', render: string = 'true', disabled: 'false'): object => {
+    return addElement('div', [addComponent('Button', { onClick, icon, severity, disabled, size: buttonSize })], { class: buttonGroupItemClass }, render)
   }
 
-  return addElementsInOuterDiv([
-    addButtonComponent('$removeNode($node.parent, $index)', '', 'pi pi-times', 'danger', 'true', '$false'),
-    addButtonComponent('$cloneNode($node.parent, $index)', '', 'pi pi-clone', '', '$displayCloneButton', '$false'),
-    addButtonComponent('$moveNodeDown($node.parent, $index)', '', 'pi pi-arrow-down', 'secondary', 'true', '$index === $node.parent.value.length -1'),
-    addButtonComponent('$moveNodeUp($node.parent, $index)', '', 'pi pi-arrow-up', 'secondary', 'true', '$index === 0'),
-  ], innerClass, outerClass, label, help, render)
+  return addElement('div', [
+    addActionButtonComponent('$moveNodeUp($node.parent, $index)', 'pi pi-arrow-up', 'secondary', '$renderMoveButtons', '$index === 0'),
+    addActionButtonComponent('$removeNode($node.parent, $index)', 'pi pi-trash', 'danger', '$displayDeleteButton', '$false'),
+    addActionButtonComponent('$cloneNode($node.parent, $index)', 'pi pi-clone', '', '$displayCloneButton', '$false'),
+    addActionButtonComponent('$addNode($node.parent, $index)', 'pi pi-plus', '', '$displayAddButton', '$false'),
+    addActionButtonComponent('$moveNodeDown($node.parent, $index)', 'pi pi-arrow-down', 'secondary', '$renderMoveButtons', '$index === $node.parent.value.length -1'),
+  ], { class: buttonGroupClass }, render)
+}
+
+function addInsertButton(label: string = 'Add Item', icon: string = 'i-lucide-plus', styleClass: string = '', buttonSize: string, render: string = 'true') {
+  return addElement('div', [
+    addComponent('Button', { onClick: '$insertNode($node)', label, icon, size: buttonSize }),
+  ], { class: styleClass }, render)
 }
 
 export const primeRepeaterDefinition: FormKitTypeDefinition = createInput(
   [
     addElement('div', [
       addList('$listName', [
-        addComponent('Button', { onClick: '$addNode($node)', label: '$addButtonLabel', size: 'small', icon: 'pi pi-plus', class: '$addButtonClass' }, '$node.children.length == 0 || $alwaysDisplayAddButton'),
+        addInsertButton('$insertButtonLabel', 'pi pi-plus', '$insertButtonClass', '$insertButtonSize', '$node.children.length == 0 || $alwaysDisplayInsertButton'),
         addListGroup([
           addElement('div', [
-            {
-              children: '$slots.default',
-            },
-            addActionButtons('$buttonsInnerClass', '$buttonsOuterClass', '$buttonsLabel'),
-          ], { class: '$itemClass' }),
-
-        ]),
+            addElement('div', [{ children: '$slots.default' }], { class: '$groupClass' }),
+            addButtonGroup('$buttonGroupClass', '$buttonGroupItemClass', '$buttonSize', '$renderButtons'),
+          ], { class: '$listItemClass' }),
+        ],
+        ),
       ], true, 'true'),
     ], { class: '$listClass' }),
 
   ],
   {
-    props: ['buttonsOuterClass', 'buttonsInnerClass', 'buttonsLabel', 'addButtonLabel', 'addButtonClass', 'newItem', 'itemClass', 'listClass', 'displayCloneButton', 'alwaysDisplayAddButton'],
+    props: ['insertButtonLabel', 'insertButtonClass', 'insertButtonSize', 'alwaysDisplayInsertButton', 'newItem', 'listClass', 'listItemClass', 'groupClass', 'hideButtonGroup', 'hideMoveButtons', 'buttonGroupClass', 'buttonGroupItemClass', 'buttonSize', 'displayCloneButton', 'displayAddButton', 'displayDeleteButton'],
     features: [addRepeaterHandler],
   },
 )
 
 function addRepeaterHandler(node: FormKitNode): void {
-  const swapElements = (array: any[], index1: number, index2: number) => {
+  const swapElements = (array: unknown[], index1: number, index2: number) => {
     const newArray = [...array]
     const temp = newArray[index1]
     newArray[index1] = newArray[index2]
@@ -50,42 +55,50 @@ function addRepeaterHandler(node: FormKitNode): void {
     return newArray
   }
   node.on('created', () => {
-    if (node.context) {
-      const newItem = node.context.newItem || {}
+    node.context.listName = node.name
+    node.context.renderButtons = !node.context.hideButtonGroup
+    node.context.insertButtonSize = node.context.insertButtonSize ? node.context.insertButtonSize : ''
+    node.context.buttonSize = node.context.buttonSize ? node.context.buttonSize : ''
+    node.context.renderMoveButtons = !node.context.hideMoveButtons
 
-      node.context.listName = node.name
-
-      node.context.removeNode = (parentNode: FormKitNode, index: number) => (): void => {
-        if (parentNode && Array.isArray(parentNode._value)) {
-          parentNode.input(parentNode._value.filter((_: any, i: number): boolean => i !== index), false)
-        }
+    node.context.insertNode = (parentNode: FormKitNode) => (): void => {
+      if (parentNode && Array.isArray(parentNode._value)) {
+        const item: unknown = node.context.newItem ? { ...node.context.newItem } : {}
+        const newArray: unknown[] = [item, ...parentNode.value]
+        parentNode.input(newArray, false)
       }
-      node.context.addNode = (parentNode: any) => (): void => {
-        if (parentNode && Array.isArray(parentNode._value)) {
-          const newArray: any[] = [...parentNode.value, newItem]
-          parentNode.input(newArray, false)
-        }
+    }
+    node.context.removeNode = (parentNode: FormKitNode, index: number) => (): void => {
+      if (parentNode && Array.isArray(parentNode._value)) {
+        parentNode.input(parentNode._value.filter((_: unknown, i: number): boolean => i !== index), false)
       }
-      node.context.cloneNode = (parentNode: any, index: number) => (): void => {
-        if (parentNode && Array.isArray(parentNode._value)) {
-          const item: any = parentNode.value[index]
-          const newArray: any[] = [...parentNode.value.slice(0, index), { ...item }, ...parentNode.value.slice(index)]
-          parentNode.input([...newArray], false)
-        }
+    }
+    node.context.addNode = (parentNode: FormKitNode, index: number) => (): void => {
+      if (parentNode && Array.isArray(parentNode._value)) {
+        const item: unknown = node.context.newItem ? { ...node.context.newItem } : {}
+        const newArray: unknown[] = [...parentNode.value.slice(0, index + 1), { ...item }, ...parentNode.value.slice(index + 1)]
+        parentNode.input([...newArray], false)
       }
-      node.context.moveNodeUp = (parentNode: any, index: number) => (): void => {
-        if (parentNode && Array.isArray(parentNode._value)) {
-          const array: any[] = [...parentNode.value]
-          if (index > 0)
-            parentNode.input(swapElements(array, index - 1, index), false)
-        }
+    }
+    node.context.cloneNode = (parentNode: FormKitNode, index: number) => (): void => {
+      if (parentNode && Array.isArray(parentNode._value)) {
+        const item: unknown = parentNode.value[index]
+        const newArray: unknown[] = [...parentNode.value.slice(0, index + 1), { ...item }, ...parentNode.value.slice(index + 1)]
+        parentNode.input([...newArray], false)
       }
-      node.context.moveNodeDown = (parentNode: any, index: number) => (): void => {
-        if (parentNode && Array.isArray(parentNode._value)) {
-          const array: any[] = [...parentNode.value]
-          if (index < array.length - 1)
-            parentNode.input(swapElements(array, index, index + 1), false)
-        }
+    }
+    node.context.moveNodeUp = (parentNode: FormKitNode, index: number) => (): void => {
+      if (parentNode && Array.isArray(parentNode._value)) {
+        const array: unknown[] = [...parentNode.value]
+        if (index > 0)
+          parentNode.input(swapElements(array, index - 1, index), false)
+      }
+    }
+    node.context.moveNodeDown = (parentNode: FormKitNode, index: number) => (): void => {
+      if (parentNode && Array.isArray(parentNode._value)) {
+        const array: unknown[] = [...parentNode.value]
+        if (index < array.length - 1)
+          parentNode.input(swapElements(array, index, index + 1), false)
       }
     }
   })
